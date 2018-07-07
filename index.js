a9483abab8d4a42ded944ef53bb33427317b3049
Min Kim <minmkim@gmail.com>
2018-07-07 01:02:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Add your functions and code here
 function destructivelyAppendKitten(name) {
-  return array.push(name)
+  return kittens.push(name)
 }
 
 function destructivelyPrependKitten(name) {
-  return array.unshift(name)
+  return kittens.unshift(name)
 }
